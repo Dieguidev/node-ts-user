d10ee8247d77b5cc58c60eda00eddfbcc0fc9039
Dieguidev <diegogaraycullas@gmail.com>
2024-03-10 08:28:29 -0500

    first commit con todo lo inicial ya configurado

---
+++ src/presentation/auth/auth.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+
+
+
+export class AuthRoutes {
+
+
+  static get routes(): Router {
+
+    const router = Router();
+    
+    // Definir las rutas
+    // router.use('/api/todos', /*TodoRoutes.routes */ );
+
+
+
+    return router;
+  }
+
+
+}
